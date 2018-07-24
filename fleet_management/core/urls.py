25ba3dc097011551bab194193285d3d6edc05deb
Md. Nahidur Rahman Saikat <nahidsaikatft40@gmail.com>
2018-07-25 03:07:15 +0600

    Removing comments from core urls

--- fleet_management/core/urls.py
+++ fleet_management/core/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path, include
 from django.conf.urls import url
-# from .views import api_root
 
 
 urlpatterns = [
-    # url(r'^$', api_root),
     path(r'', include('core.fleet.urls')),
     path(r'', include('core.vehicle.urls')),
 ]
