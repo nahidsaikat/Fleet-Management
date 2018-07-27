f2476ef6039984e3c885811e7da7aa918cfc3414
Md. Nahidur Rahman Saikat <nahidsaikatft40@gmail.com>
2018-07-27 12:06:58 +0600

    Updating model and serializer of requisitions.

--- fleet_management/core/requisition/models.py
+++ fleet_management/core/requisition/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-from core.fleet.models import Fleet
+from core.vehicle.models import Vehicle
 
 
 class Requisition(models.Model):
-    serial = models.CharField(max_length=255, null=False)
-    fleet = models.ForeignKey(Fleet, on_delete=models.CASCADE)
+    vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE)
+    from_date = models.DateField(null=False)
+    from_time = models.TimeField(null=False)
+    to_date = models.DateField(null=False)
+    to_time = models.TimeField(null=False)
     status = models.CharField(max_length=30, null=False)
-    driver = models.ForeignKey(User, on_delete=models.CASCADE)
+    requisition_by = models.ForeignKey(User, on_delete=models.CASCADE)
--- fleet_management/core/requisition/serializers.py
+++ fleet_management/core/requisition/serializers.py
@@ -5,4 +5,4 @@ from .models import Requisition
 class RequisitionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Requisition
-        fields = ('id', 'serial', 'fleet', 'status', 'driver')
+        fields = ('id', 'vehicle', 'from_date', 'from_time', 'to_date', 'to_time', 'status', 'requisition_by')
