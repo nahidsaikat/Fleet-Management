7a132dad7f04993b08eaf3aef9f6d38c3b795b9a
Md. Nahidur Rahman Saikat <nahidsaikatft40@gmail.com>
2018-07-31 01:07:43 +0600

    setting fields in requisition log model

--- fleet_management/core/requisition/requisition_log/models.py
+++ fleet_management/core/requisition/requisition_log/models.py
@@ -1,14 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-from core.vehicle.models import Vehicle
+from core.requisition.models import Requisition
 
 
-class Requisition(models.Model):
-    vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE)
-    from_date = models.DateField(null=False)
-    from_time = models.TimeField(null=False)
-    to_date = models.DateField(null=False)
-    to_time = models.TimeField(null=False)
-    status = models.CharField(max_length=30, null=False)
-    requisition_by = models.ForeignKey(User, on_delete=models.CASCADE)
+class RequisitionLog(models.Model):
+    requisition = models.ForeignKey(Requisition, on_delete=models.CASCADE)
+    from_status = models.CharField(max_length=30)
+    to_status = models.CharField(max_length=30)
+    date_time = models.DateTimeField(null=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
