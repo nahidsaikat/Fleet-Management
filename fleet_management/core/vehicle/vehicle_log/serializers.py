b5a16697aa77af29ac3faffb3b9e23ddcc4836f4
Md. Nahidur Rahman Saikat <nahidsaikatft40@gmail.com>
2018-08-02 22:33:17 +0600

    fixing fields name of serializer.

--- fleet_management/core/vehicle/vehicle_log/serializers.py
+++ fleet_management/core/vehicle/vehicle_log/serializers.py
@@ -5,4 +5,4 @@ from .models import VehicleLog
 class VehicleLogSerializer(serializers.ModelSerializer):
     class Meta:
         model = VehicleLog
-        fields = ('id', 'requisition', 'from_status', 'to_status', 'date_time', 'user')
+        fields = ('id', 'vehicle', 'driver', 'from_status', 'to_status', 'remarks')
