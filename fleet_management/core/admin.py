c18821357c7889fabc426594fe68b65727219874
Md. Nahidur Rahman Saikat <nahidsaikatft40@gmail.com>
2018-08-02 23:10:04 +0600

    registering VehicleLog and RequisitionLog

--- fleet_management/core/admin.py
+++ fleet_management/core/admin.py
@@ -2,13 +2,17 @@ from django.contrib import admin
 
 from .fleet.models import Fleet
 from .vehicle.models import Vehicle
+from .vehicle.vehicle_log.models import VehicleLog
 from .requisition.models import Requisition
+from .requisition.requisition_log.models import RequisitionLog
 from .comment.models import Comment
 from .notification.models import Notification
 
 
 admin.site.register(Fleet)
 admin.site.register(Vehicle)
+admin.site.register(VehicleLog)
 admin.site.register(Requisition)
+admin.site.register(RequisitionLog)
 admin.site.register(Comment)
 admin.site.register(Notification)
