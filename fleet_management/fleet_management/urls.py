1520df8b70dbce79c3352b6c9b001b24fe912e4f
nahidsaikat <nahidsaikatft40@gmail.com>
2018-08-21 23:51:37 +0600

    Deleting un-necessary files and updating index.html file

--- fleet_management/fleet_management/urls.py
+++ fleet_management/fleet_management/urls.py
@@ -20,7 +20,7 @@ from django.views.generic.base import TemplateView
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),
     path('', include('django.contrib.auth.urls')),
     path('', include('core.urls')),
 ]
